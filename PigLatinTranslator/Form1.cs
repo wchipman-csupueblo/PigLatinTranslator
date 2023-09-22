3b376b325dcb14a1fb8a85512617a90010870e6e
bill chipman <wrchipman@gmail.com>
2023-09-21 21:54:29 -0400

    Start of translate button

--- PigLatinTranslator/Form1.cs
+++ PigLatinTranslator/Form1.cs
@@ -27,5 +27,18 @@ namespace PigLatinTranslator
             txtEnglish.Text = "";
             txtPigLatin.Text = "";
         }
+
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txtEnglish.Text))
+            {
+                //Do all the rules
+            }
+            else
+            {
+                MessageBox.Show("You must enter some text to translate.", "Enter");
+                txtEnglish.Focus();
+            }
+        }
     }
 }
